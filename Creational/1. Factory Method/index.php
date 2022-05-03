252339254936fbdcb3434482da6896a71d3fdd5e
Daniel Vasquez <victor.vasquez@wizeline.com>
2022-05-03 18:14:03 +0200

    change Factory method to explain it better

--- Creational/1. Factory Method/index.php
+++ Creational/1. Factory Method/index.php
@@ -11,11 +11,9 @@ class PlaneTransport implements  Transport {
     public function ready(): void {
         echo "Courier ready to be sent to the plane \n";
     }
-
     public function dispatch(): void {
         echo "Courier is on its way to the plane \n";
     }
-
     public function deliver(): void {
         echo "Courier from the plane is delivered to you \n";
     }
@@ -25,52 +23,70 @@ class TruckTransport implements Transport {
     public function ready(): void {
         echo "Ready to be sent to the truck \n";
     }
-
     public function dispatch(): void {
         echo "On its way to the truck \n";
     }
-
     public function deliver(): void {
         echo "From the truck is delivered to you \n";
     }
 }
 
-//creational object
-abstract class Courier {
-    abstract function  getCourierTransport(): Transport;
+class ShipTransport implements  Transport {
+    public function ready(): void {
+        echo "Ready to be sent to the ship \n";
+    }
+    public function dispatch(): void {
+        echo "From the ship is its way \n";
+    }
+    public function deliver(): void {
+        echo "From the ship is delivered to you \n";
+    }
+}
 
-    public function sendCourier() {
-        $transport = $this->getCourierTransport();
+interface ITransportFactory {
+    public static function getTransport(String $type): Transport;
+}
+
+class TransportFactory implements  ITransportFactory {
+    public static function getTransport(String $type): Transport
+    {
+        switch ($type) {
+            case "Air":
+                return new PlaneTransport();
+                break;
+            case "Ground":
+                return new TruckTransport();
+                break;
+            case "Sea":
+                return new ShipTransport();
+                break;
+            default:
+                throw new \Exception('courier transport not associated \n');
+        }
+    }
+}
+
+//client code
+function deliverCourier(String $type){
+    try {
+        $transport = TransportFactory::getTransport($type);
         $transport->ready();
         $transport->dispatch();
         $transport->deliver();
+    } catch (Exception $e) {
+        echo "The transport can't be retrieved \n";
     }
-}
-
-class AirCourier extends Courier {
-
-    function getCourierTransport(): Transport {
-        return new PlaneTransport();
-    }
-}
-
-class GroundCourier extends Courier {
-
-    function getCourierTransport(): Transport {
-        return new TruckTransport();
-    }
-}
-
-
-//client code
-function deliverCourier(Courier $courier){
-    $courier->sendCourier();
 }
 
 
 //creates specific instances
 echo "From the air ... \n";
-deliverCourier(new AirCourier());
-
+deliverCourier("Air");
+echo "-------------------------\n";
 echo "From the ground ... \n";
-deliverCourier(new GroundCourier());
+deliverCourier("Ground");
+echo "-------------------------\n";
+deliverCourier("Sea");
+echo "-------------------------\n";
+deliverCourier("Nada");
+echo "-------------END-------\n";
